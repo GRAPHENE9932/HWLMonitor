7942ae9e1a27f6bcc43e6a2b852eae0a8d867b38
Artem Klimiński <graphene64@gmail.com>
2026-04-04 13:21:21 +0200

    feat: Display the illuminance in kilolux after a certain threshold

--- src/gui/mode_statistics.c
+++ src/gui/mode_statistics.c
@@ -7,18 +7,35 @@
 
 #define TEXT_COLOR ST7735_COLOR(29u, 59u, 29u)
 #define MILLI_THRESHOLD 1000u
+#define KILO_THRESHOLD 10'000'000u
 
-struct lx_val {
+struct lx_val_text {
     struct gui_text text;
     char buf[6u];
 };
 
-struct lx_unit {
+struct lx_unit_text {
     struct gui_text text;
     char buf[3u];
 };
 
-static void lx_val_init(struct lx_val* lv) {
+enum lx_unit : uint8_t {
+    UNIT_MILLILUX,
+    UNIT_LUX,
+    UNIT_KILOLUX,
+};
+
+static enum lx_unit choose_unit(uint32_t mlx) {
+    if (mlx < MILLI_THRESHOLD) {
+        return UNIT_MILLILUX;
+    } else if (mlx >= KILO_THRESHOLD) {
+        return UNIT_KILOLUX;
+    } else {
+        return UNIT_LUX;
+    }
+}
+
+static void lx_val_text_init(struct lx_val_text* lv) {
     gui_text_init(&lv->text, 2u);
     for (uint32_t i = 0u; i < sizeof(lv->buf); ++i) {
         lv->buf[i] = '-';
@@ -31,20 +48,31 @@ static void lx_val_init(struct lx_val* lv) {
     lv->text.height_cutoff = FONT_HEIGHT;
 }
 
-static void lx_val_update(struct lx_val* lv, uint32_t mlx) {
+static void lx_val_text_update(
+    struct lx_val_text* lv, uint32_t mlx, enum lx_unit unit) {
     uint8_t decimals = 0u;
     uint32_t int_val = 0u;
     uint32_t dec_val = 0u;
 
-    if (mlx < MILLI_THRESHOLD) {
+    switch (unit) {
+    case UNIT_MILLILUX:
         decimals = 0u;
         int_val = mlx;
         dec_val = 0u;
-    } else {
+        break;
+    case UNIT_LUX:
         const uint8_t digits_lx = max_i32(1, (int32_t)u32_log10(mlx) - 2);
         decimals = clamp_i32(5 - digits_lx, 0, 3);
         int_val = mlx / 1000u;
         dec_val = (mlx - int_val * 1000u) / u32_pow10(max_i32(0, 3 - decimals));
+        break;
+    case UNIT_KILOLUX:
+        const uint8_t digits_klx = max_i32(1, (int32_t)u32_log10(mlx) - 5);
+        decimals = clamp_i32(5 - digits_klx, 0, 3);
+        int_val = mlx / 1'000'000u;
+        dec_val =
+            (mlx - int_val * 1'000'000u) / u32_pow10(max_i32(0, 6 - decimals));
+        break;
     }
 
     uint32_t off = i32_to_str(lv->buf, sizeof(lv->buf), int_val);
@@ -56,11 +84,11 @@ static void lx_val_update(struct lx_val* lv, uint32_t mlx) {
     lv->text.len = off;
 }
 
-static void lx_val_output(struct lx_val* lv) {
+static void lx_val_text_output(struct lx_val_text* lv) {
     gui_draw_text(&lv->text);
 }
 
-static void lx_unit_init(struct lx_unit* lu) {
+static void lx_unit_text_init(struct lx_unit_text* lu) {
     gui_text_init(&lu->text, 2u);
     lu->buf[0] = 'l';
     lu->buf[1] = 'x';
@@ -73,26 +101,35 @@ static void lx_unit_init(struct lx_unit* lu) {
     lu->text.height_cutoff = FONT_HEIGHT;
 }
 
-static void lx_unit_update(struct lx_unit* lu, uint32_t mlx) {
-    if (mlx < MILLI_THRESHOLD) {
+static void lx_unit_text_update(struct lx_unit_text* lu, enum lx_unit unit) {
+    switch (unit) {
+    case UNIT_MILLILUX:
         lu->buf[0] = 'm';
         lu->buf[1] = 'l';
         lu->buf[2] = 'x';
         lu->text.len = 3u;
-    } else {
+        break;
+    case UNIT_LUX:
         lu->buf[0] = 'l';
         lu->buf[1] = 'x';
         lu->text.len = 2u;
+        break;
+    case UNIT_KILOLUX:
+        lu->buf[0] = 'k';
+        lu->buf[1] = 'l';
+        lu->buf[2] = 'x';
+        lu->text.len = 3u;
+        break;
     }
 }
 
-static void lx_unit_output(struct lx_unit* lu) {
+static void lx_unit_text_output(struct lx_unit_text* lu) {
     gui_draw_text(&lu->text);
 }
 
 void mode_statistics_start(void) {
-    struct lx_val lx_val = { 0 };
-    struct lx_unit lx_unit = { 0 };
+    struct lx_val_text lx_val_text = { 0 };
+    struct lx_unit_text lx_unit_text = { 0 };
 
     opt4001_init();
     opt4001_start_cont();
@@ -100,14 +137,15 @@ void mode_statistics_start(void) {
     gui_clear_bg();
     status_bar_set_text("Statistics");
     
-    lx_val_init(&lx_val);
-    lx_unit_init(&lx_unit);
+    lx_val_text_init(&lx_val_text);
+    lx_unit_text_init(&lx_unit_text);
 
     while (true) {
         const uint32_t mlx = opt4001_read_mlx();
-        lx_val_update(&lx_val, mlx);
-        lx_val_output(&lx_val);
-        lx_unit_update(&lx_unit, mlx);
-        lx_unit_output(&lx_unit);
+        const enum lx_unit unit = choose_unit(mlx);
+        lx_val_text_update(&lx_val_text, mlx, unit);
+        lx_val_text_output(&lx_val_text);
+        lx_unit_text_update(&lx_unit_text, unit);
+        lx_unit_text_output(&lx_unit_text);
     }
 }
