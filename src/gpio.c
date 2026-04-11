21540ab135145623972b3fe65a0ef1726b39e9f9
Artem Klimiński <graphene64@gmail.com>
2026-04-11 20:55:13 +0200

    fix: Fix GPIO alternate function setting for pins [8:15]

--- src/gpio.c
+++ src/gpio.c
@@ -37,8 +37,8 @@ void gpio_init(GPIO_TypeDef* gpio, uint8_t pin, uint32_t attrs, uint8_t af) {
     gpio->PUPDR &= ~(GPIO_PUPDR_PUPDR0_Msk << (pin * 2));
     gpio->PUPDR |= ((attrs & 0xFF000000) >> 24) << (pin * 2);
 
-    gpio->AFR[af / 8] &= ~(GPIO_AFRL_AFSEL0_Msk << (pin % 8 * 4));
-    gpio->AFR[af / 8] |= (uint32_t)af << (pin % 8 * 4);
+    gpio->AFR[pin / 8] &= ~(GPIO_AFRL_AFSEL0_Msk << (pin % 8 * 4));
+    gpio->AFR[pin / 8] |= (uint32_t)af << (pin % 8 * 4);
 
     gpio->MODER &= ~(GPIO_MODER_MODER0_Msk << (pin * 2));
     gpio->MODER |= (attrs & 0x000000FF) << (pin * 2);
