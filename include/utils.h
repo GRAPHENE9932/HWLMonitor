290ebfe95e5a09de844ccae03dfec1d0a7216684
Artem Klimiński <graphene64@gmail.com>
2026-04-15 16:28:30 +0200

    feat: Expand min/max functions for other integer types

--- include/utils.h
+++ include/utils.h
@@ -19,11 +19,47 @@ static inline uint32_t min_u32(uint32_t a, uint32_t b) {
     return a < b ? a : b;
 }
 
+static inline uint16_t min_u16(uint16_t a, uint16_t b) {
+    return a < b ? a : b;
+}
+
+static inline uint8_t min_u8(uint8_t a, uint8_t b) {
+    return a < b ? a : b;
+}
+
+static inline uint32_t max_u32(uint32_t a, uint32_t b) {
+    return a > b ? a : b;
+}
+
+static inline uint16_t max_u16(uint16_t a, uint16_t b) {
+    return a > b ? a : b;
+}
+
+static inline uint8_t max_u8(uint8_t a, uint8_t b) {
+    return a > b ? a : b;
+}
+
 static inline int32_t max_i32(int32_t a, int32_t b) {
     return a > b ? a : b;
 }
 
+static inline int16_t max_i16(int16_t a, int16_t b) {
+    return a > b ? a : b;
+}
+
+static inline int8_t max_i8(int8_t a, int8_t b) {
+    return a > b ? a : b;
+}
+
 static inline int32_t min_i32(int32_t a, int32_t b) {
+    return a < b ? a : b;
+}
+
+static inline int16_t min_i16(int16_t a, int16_t b) {
+    return a < b ? a : b;
+}
+
+static inline int8_t min_i8(int8_t a, int8_t b) {
     return a < b ? a : b;
 }
 
